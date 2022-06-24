fcd6ab8d1101827a59286c7bfe80d8367442c433
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-25 04:17:32 +0530

    Add project files.

---
+++ Private Constructor SingletanDP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Private_Constructor_SingletanDP
+{
+    class MyClass
+    {
+       private MyClass()
+        {
+            Console.WriteLine("Private Constructor :");
+        }
+     
+
+        private static MyClass my;
+
+        public static MyClass GetClass()
+        {
+            if (my==null)
+            {
+                my = new MyClass();
+            }
+                return my;
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = MyClass.GetClass();
+            
+           
+        }
+    }
+}
